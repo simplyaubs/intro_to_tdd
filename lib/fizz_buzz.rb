de6c052cd3e9ea8ce613507851206a8833059ffb
Aubrey Howell <simplyaubs@gmail.com>
2014-03-10 16:38:38 -0600

    Changed name of files to add underscore

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,13 @@
+class FizzBuzz
+
+  my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+
+  puts my_array
+
+  def is_multiple_of?(divisible_by, number)
+    number % divisible_by == 0
+  end
+
+
+
+end
---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,11 @@
+require "rspec/core"
+
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  it "It will print out numbers 1 through 10" do
+
+  expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+  end
+end
+
