3634488680cec77320be500970b1742cf1b9fcde
Aubrey Howell <simplyaubs@gmail.com>
2014-03-10 20:07:23 -0600

    Runs numbers 1 and 2 with spec

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,11 @@
 class FizzBuzz
 
-  my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
-
-  puts my_array
-
-  def is_multiple_of?(divisible_by, number)
-    number % divisible_by == 0
+  def initialize(number)
+    @number = number
   end
 
-
+  def arrayify
+    (1..@number).to_a
+  end
 
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,11 +1,27 @@
 require "rspec/core"
-
 require "fizz_buzz"
 
-describe "fizz_buzz" do
-  it "It will print out numbers 1 through 10" do
-
-  expected = [1, 2, "Fizz", 4, "Buzz", 6, 7, 8, "Fizz", "Buzz",]
+describe "FizzBuzz" do
+  it "Returns 1 when passed a 1" do
+    fizzy = FizzBuzz.new(1)
+    results = fizzy.arrayify
+    expect(results).to eq [1]
   end
+
+  it "Returns 1, 2 when passed a 2" do
+    fizzy = FizzBuzz.new(2)
+    results = fizzy.arrayify
+    expect(results).to eq [1, 2]
+  end
+
+  it "Returns 1, 2, Fizz when passed a 3" do
+    pending
+    fizzy = FizzBuzz.new(3)
+    results = fizzy.arrayify
+    expect(results).to eq [1, 2, 'Fizz']
+  end
+
+
+
 end
 
